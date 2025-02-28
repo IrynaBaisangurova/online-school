76e114486bbaba07fe94b3036e553b61ae1b5e87
Iryna <laudaeren@ukr.net>
2025-02-28 22:09:01 +0200

    add card

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import './is/hero.js';
-import './is/counter.js';
-import './is/mod.js';
-import './is/about-me.js';
-import './is/reviews.js';
-import './is/swiper.js'
+import './js/hero.js';
+import './js/counter.js';
+import './js/mod.js';
+import './js/about-me.js';
+import './js/reviews.js';
+import './js/swiper.js'
 
 
 

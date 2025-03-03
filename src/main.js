091a6a9d71f563cb62465588205ee49ee71dfb3f
Iryna <laudaeren@ukr.net>
2025-03-03 22:44:56 +0200

    add cart

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import './js/counter.js';
 import './js/mod.js';
 import './js/about-me.js';
 import './js/reviews.js';
-import './js/swiper.js'
+import './js/swiper.js';
+import './js/app.js';
+import './js/checkout.js';
+
 
 
 
